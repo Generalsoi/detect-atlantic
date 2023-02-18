e141072316bd2f3261f8842743c4412821094ebb
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-18 07:24:59 +0100

    fix: navbar dropdown implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import Dlogo from "../assets/images/Dlogo.png";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 type sublink = {
   name: string;
@@ -18,6 +19,7 @@ type navLink = {
 
 export const Navbar: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [dropdown, setDropDown] = useState<boolean>(false);
 
   const navLinks: navLink[] = [
     { index: 1, name: "Home", link: "/", sublinks: [] },
@@ -40,6 +42,13 @@ export const Navbar: FC = () => {
     setOpen(!open);
   };
 
+  const handleDropDown = (id: number): void => {
+    if (id === 3) {
+      setDropDown(!dropdown);
+    }
+    return;
+  };
+
   return (
     <div className="text-[#fff] md:flex items-start justify-between px-4 md:px-20 py-4 fixed top-0 bg-[#000000] w-full z-20 transition-all ease-in-out duration-300">
       <img src={Dlogo} alt="d-logo" />
@@ -56,7 +65,7 @@ export const Navbar: FC = () => {
       </span>
 
       <span
-        className={`flex flex-col  ${open ? "block h-96" : "hidden md:flex"}`}
+        className={`flex flex-col  ${open ? "block h-fit" : "hidden md:flex"}`}
       >
         {navLinks.map((navlink) => (
           <NavLink
@@ -68,8 +77,24 @@ export const Navbar: FC = () => {
                 : "font-ibmPlexMono mt-16 md:mt-0 border border-x-0 border-t-0 border-b-1 border-white md:w-24 text-md md:text-xs"
             }
           >
-            {navlink.name}
-            {}
+            <span
+              onClick={() => handleDropDown(navlink.index)}
+              className="flex items-center gap-1"
+            >
+              {navlink.name}{" "}
+              {navlink.index === 3 && (
+                <IoMdArrowDropdown className="text-[#FF6108]" />
+              )}
+            </span>
+            {navlink.index === 3 && dropdown && (
+              <div className="flex flex-col ml-10">
+                {navlink.sublinks.map((sublink) => (
+                  <a href={sublink.sublink} className="mt-4 md:mt-1">
+                    {sublink.name}
+                  </a>
+                ))}
+              </div>
+            )}
           </NavLink>
         ))}
       </span>
