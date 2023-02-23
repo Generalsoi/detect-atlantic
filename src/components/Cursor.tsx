f5ad2d3f820d0d64c9a29b7a296d2e564b3d5507
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-23 20:04:47 +0100

    fix: minor updates

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,19 +1,46 @@
 import React, { FC, useEffect, useState } from "react";
 
-export const Cursor: FC = () => {
-  const [cursorx, setCursorx] = useState<number>();
-  const [cursory, setCursory] = useState<number>();
+type cursorPosition = {
+  x: number;
+  y: number;
+};
 
-  window.addEventListener("mousemove", (e) => {
-    setCursorx(e.pageX);
-    setCursory(e.pageY);
-  });
+export const Cursor: FC = () => {
+  const [position, setPosition] = useState<cursorPosition>({ x: 0, y: 0 });
+  // const [cursorx, setCursorx] = useState<number>();
+  // const [cursory, setCursory] = useState<number>();
+
+  // window.addEventListener("mousemove", (e) => {
+  //   setCursorx(e.pageX);
+  //   setCursory(e.pageY);
+  // });
+
+  useEffect(() => {
+    const addEventListeners = () => {
+      document.addEventListener("mousemove", mMove);
+    };
+
+    const removeEventListeners = () => {
+      document.removeEventListener("mousemove", mMove);
+    };
+
+    const mMove = (e: any) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    addEventListeners();
+    return () => removeEventListeners();
+  }, []);
 
   return (
     <div
       className="cursor-dot-outline"
       id="cursor"
-      style={{ left: cursorx + "px", top: cursory + "px" }}
+      // style={{ left: cursorx + "px", top: cursory + "px" }}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
     >
       {/* <div className="cursor-dot"></div> */}
     </div>
