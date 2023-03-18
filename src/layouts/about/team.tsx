8f44207fda40b521e2a4deead0e089dce6649e26
Generalsoi <ikhinobelesuccess@gmail.com>
2023-03-18 07:58:37 +0100

    fix: minor updates

--- src/layouts/about/team.tsx
+++ src/layouts/about/team.tsx
@@ -10,18 +10,18 @@ export const Team = () => {
         meet the team
       </h1>
 
-      <div className="md:flex items-start justify-center gap-20 mt-20">
+      <div className="px-8 md:px-0 md:flex items-start justify-center gap-20 mt-20">
         {team.map((member, index) => (
           <motion.div
             key={index}
-            className="md:w-[23%] mb-20"
+            className="w-full md:w-[23%] mb-20"
             whileInView={{ y: 0, opacity: 1 }}
             initial={{ y: 100, opacity: 0 }}
             transition={{ type: "spring", stiffness: 20 }}
           >
             <img src={member.image} alt={member.image} />
             <a href={member.linkedin} target="_blank" rel="noreferrer">
-              <h4 className="my-10 uppercase text-xl md:text-3xl hover:underline transition-all ease-in-out duration-300 ">
+              <h4 className="my-6 uppercase text-xl md:text-3xl hover:underline transition-all ease-in-out duration-300 text-center">
                 {member.name}
               </h4>
             </a>
