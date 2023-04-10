fc2c7f17f35c1051b7c7cf88274461d4fa81eaf7
Generalsoi <ikhinobelesuccess@gmail.com>
2023-04-10 06:41:56 +0100

    fix: minor updates

--- src/layouts/solutions/constructionMonitoringScroll.tsx
+++ src/layouts/solutions/constructionMonitoringScroll.tsx
@@ -5,7 +5,7 @@ import ArrowRight from "../../assets/images/arrow-right.png";
 export const ConstructionMonitoringScroll: FC = () => {
   return (
     <div className="hidden lg:block relative  overflow-hidden h-40 font-ibmPlexMono text-white my-10">
-      <div className="absolute w-[200%] top-0 left-0 right-0 flex gap-4 h-24 border-none">
+      <div className="absolute w-[200%] top-0 left-0 right-0 flex h-24 border-none">
         <section className="flex items-center gap-6 py-4 w-[50%] animate-swipe">
           <img src={ArrowRight} alt="arrow-right" className="ml-6" />
           <p className=" text-lg text-[#AAAAAA] font-bold">Growth monitoring</p>
@@ -24,19 +24,16 @@ export const ConstructionMonitoringScroll: FC = () => {
           <p className=" text-lg text-[#AAAAAA] font-bold">Growth monitoring</p>
         </section>
         <section className="flex items-center gap-6 py-4 w-[50%] animate-swipe">
-          <img src={ArrowRight} alt="arrow-right" className="ml-6" />
+          <img src={ArrowRight} alt="arrow-right" className="" />
           <p className=" text-lg text-[#AAAAAA] font-bold">
             Predictive analysis
           </p>
-
           <img src={ArrowRight} alt="arrow-right" className="ml-6" />
           <p className=" text-lg text-[#AAAAAA] font-bold">
             Construction monitoring
           </p>
-
           <img src={ArrowRight} alt="arrow-right" />
           <p className=" text-lg text-[#AAAAAA] font-bold">Access planning</p>
-
           <img src={ArrowRight} alt="arrow-right" className="ml-6" />
           <p className=" text-lg text-[#AAAAAA] font-bold">Growth monitoring</p>
           <img src={ArrowRight} alt="arrow-right" className="ml-6" />
