2efd39b50d4f876ac5fd4e460ae99d1c7b9d3255
Generalsoi <ikhinobelesuccess@gmail.com>
2023-04-10 05:49:35 +0100

    fix: modified the home page video

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -34,34 +34,7 @@ export const Homepage = () => {
       exit={{ opacity: 0, transition: { duration: 0.4 } }}
     >
       <Navbar />
-      {/* <div className="text-left md:flex flex-col lg:h-screen items-center justify-center text-white px-4 py-10 mt-16 md:mt-20">
-        <span>
-          <motion.h1
-            className="text-5xl md:text-6xl lg:text-8xl uppercase font-bold"
-            whileInView={{ y: 0, opacity: 1 }}
-            initial={{ y: 100, opacity: 0 }}
-            transition={{ type: "spring", stiffness: 30 }}
-          >
-            We see it
-          </motion.h1>
-          <motion.h1
-            className="text-5xl md:text-6xl lg:text-8xl uppercase font-bold"
-            whileInView={{ y: 0, opacity: 1 }}
-            initial={{ y: 100, opacity: 0 }}
-            transition={{ type: "spring", stiffness: 20 }}
-          >
-            before it
-          </motion.h1>
-          <motion.h1
-            className="text-5xl md:text-6xl lg:text-8xl uppercase font-bold"
-            whileInView={{ y: 0, opacity: 1 }}
-            initial={{ y: 100, opacity: 0 }}
-            transition={{ type: "spring", stiffness: 20 }}
-          >
-            happens
-          </motion.h1>
-        </span>
-      </div> */}
+
       <motion.div
         className="px-4 lg:px-0 w-full flex items-center relative top-20 overflow-hidden"
         whileInView={{ y: 0, opacity: 1 }}
@@ -71,24 +44,6 @@ export const Homepage = () => {
         <video autoPlay muted playsInline loop>
           <source src={HomePageVid} type="video/mp4" />
         </video>
-        {/* <div className="md:absolute md:top-[8rem] lg:top-[20rem] lg:px-40 w-full hidden md:flex items-center justify-center ">
-          <div className="bg-black p-8 rounded-lg">
-            <h1 className=" lg:text-2xl text-white">
-              Detect can provide you with insightful actions to
-            </h1>
-            <h1 className=" lg:text-2xl text-white">
-              improve your assets' integrity at a lower cost.
-            </h1>
-
-            <h1 className=" lg:text-2xl text-white">
-              We are flexible in our support by offering your
-            </h1>
-            <h1 className=" lg:text-2xl text-white">
-              selection of data collection, annotation, and
-            </h1>
-            <h1 className=" lg:text-2xl text-white">analysis.</h1>
-          </div>
-        </div> */}
       </motion.div>
 
       <div className="text-white mt-28 py-10  text-center">
@@ -126,54 +81,6 @@ export const Homepage = () => {
       <Infrastructurescroll />
 
       <WhatWeDo />
-
-      {/* <div className="md:flex px-6 md:pl-20 md:pr-0 pb-8 mt-32 md:mb-40 ">
-        <div className="md:w-[50%]">
-          {servicesList.map((service) => (
-            <div
-              key={service.index}
-              className={`py-8 border border-x-0 border-red cursor-pointer border-neutral-400 ${
-                service.index === 4 ? "border-b-1" : "border-b-0"
-              } border-[#AAAAAA] transition ease-linear duration-500 text-white`}
-              onClick={() => handleOpenAccordion(service.index)}
-              style={{
-                borderTopWidth: "0.1px",
-                borderColor: "#AAAAAA",
-              }}
-            >
-              <h4 className="flex items-center gap-2 uppercase text-2xl">
-                {service.name}
-                {currentId === service.index ? (
-                  <MdKeyboardArrowDown />
-                ) : (
-                  <MdKeyboardArrowRight />
-                )}
-              </h4>
-              {currentId === service.index && (
-                <motion.span
-                  initial={{ opacity: 0, y: 0 }}
-                  animate={{ opacity: 1, y: "100%" }}
-                  transition={{ type: "spring", stiffness: 20 }}
-                >
-                  <p className="my-8 text-sm">{service.description}</p>
-
-                  <a
-                    href={service.link}
-                    className="text-[#FF6108] uppercase flex items-center gap-3"
-                  >
-                    Learn More <img src={ArrowRight} alt="arrow-right" />
-                  </a>
-                </motion.span>
-              )}
-            </div>
-          ))}
-        </div>
-        <img
-          className="hidden md:block w-[50%] h-[34rem]"
-          src={MediaTwo}
-          alt="mediatwo"
-        />
-      </div> */}
 
       <div className="md:flex px-6 md:px-10 lg:px-20  pb-8 mt-32 md:mb-40  ">
         <div className="w-full">
